8def6cb29bccc2cebee171499918aa533f37a794
pawelzareba <pzareba22@gmail.com>
2024-02-24 12:23:38 +0100

    Disable git blame on startup

--- nvim/lua/plugins/gitblame.lua
+++ nvim/lua/plugins/gitblame.lua
@@ -4,6 +4,7 @@ return {
   "f-person/git-blame.nvim",
   opts = function ()
         local git_blame = require("gitblame")
+        git_blame.disable()
         map("n", "<leader>gb", git_blame.toggle)
   end
 }
