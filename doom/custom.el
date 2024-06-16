1ae59ffd96aae27aa68fb0074108771f8a96e131
pawelzareba <pzareba22@gmail.com>
2024-06-16 13:41:40 +0200

    Doom emacs safe themes

--- doom/custom.el
+++ doom/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages '(evil-collection org-habit-stats org-journal nov)))
 (custom-set-faces
